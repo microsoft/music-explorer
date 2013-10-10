892ec5f7baebe633f070368741777ac0bda4fa38
Mikko Piipponen <mikko.piipponen@futurice.com>
2013-10-10 13:27:28 +0300

    replace Buttons in ArtistPivot with TiltGrids

---
+++ MusicExplorer/TiltGrid.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Phone.Controls;
+using System.Windows.Controls;
+
+namespace MusicExplorer
+{
+    public class TiltGrid : Grid
+    {
+        public TiltGrid()
+        {
+            if (!TiltEffect.TiltableItems.Contains(typeof(TiltGrid)))
+            {
+                TiltEffect.TiltableItems.Add(typeof(TiltGrid));
+            }
+        }
+    }
+}
