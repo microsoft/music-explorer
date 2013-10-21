1b3158770f991960144b011a7d29f3f2ac1d1e9d
Mikko Piipponen <mikko.piipponen@futurice.com>
2013-10-21 11:00:41 +0300

    Update copyright and license descriptions

--- MusicExplorer/TiltGrid.cs
+++ MusicExplorer/TiltGrid.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.Phone.Controls;
+﻿/**
+ * Copyright (c) 2013 Nokia Corporation. All rights reserved.
+ *
+ * Nokia, Nokia Connecting People, Nokia Developer, and HERE are trademarks
+ * and/or registered trademarks of Nokia Corporation. Other product and company
+ * names mentioned herein may be trademarks or trade names of their respective
+ * owners.
+ *
+ * See the license text file delivered with this project for more information.
+ */
+
+using Microsoft.Phone.Controls;
 using System.Windows.Controls;
 
 namespace MusicExplorer
